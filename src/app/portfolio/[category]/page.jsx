c1079acbf8b6950371599bfed171f9d55b5df13d
Fundream18 <fandriydb@gmail.com>
2023-07-31 05:54:53 +0800

    add portofolio data

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,29 +1,39 @@
 import Button from "@/components/button/Button";
 import styles from "./page.module.css";
-
 import React from "react";
 import Image from "next/image";
+import { items } from "./data";
+import { notFound } from "next/navigation";
+
+const getData = (cat) => {
+  const data = items[cat];
+
+  if (data) {
+    return data;
+  }
+
+  return notFound();
+};
 
 const Category = ({ params }) => {
+  const datas = getData(params.category);
   return (
     <div className={styles.container}>
       <h1 className={styles.catTitle}>{params.category}</h1>
-      <div className={styles.item}>
-        <div className={styles.content}>
-          <h1>Test</h1>
-          <p>Desc</p>
-          <Button text="See more" url="#" />
-        </div>
-        <div className={styles.imgContainer}>
-          <Image
-            className=""
-            src="https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-            width={10}
-            height={10}
-            alt=""
-          />
-        </div>
-      </div>
+      {datas.map((data) => {
+        return (
+          <div className={styles.item} key={data.id}>
+            <div className={styles.content}>
+              <h1>{data.title}</h1>
+              <p>{data.desc}</p>
+              <Button text="See more" url="#" />
+            </div>
+            <div className={styles.imgContainer}>
+              <Image className="" src={data.image} fill={true} alt="" />
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
